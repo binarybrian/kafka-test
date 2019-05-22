c6d13e4f81597a332a291a077115584f7cb893d4
Brian Merrill <bmerrill@proofpoint.com>
2019-05-22 09:59:10 -0600

    Vault reader

--- src/main/scala/com/proofpoint/VaultReader.scala
+++ src/main/scala/com/proofpoint/VaultReader.scala
@@ -3,10 +3,11 @@ package com.proofpoint
 import scala.sys.process._
 
 object VaultReader {
+  val resource = "sherlock.conf"
   private val regex = "export ([^=]+)='([^']+)'".r
   def readVaultProperties(env: String): Unit = {
-    println("loading variables from vault...")
-    val result = s"../do-vault/do-vault.sh --entity configfile --conf ../do-vault/config/$env.conf --files src/main/resources/service.conf".!!
+    println(s"Loading $resource variables from $env vault...")
+    val result = s"../do-vault/do-vault.sh --entity configfile --conf ../do-vault/config/$env.conf --files src/main/resources/$resource".!!
     regex.findAllIn(result).matchData.foreach {
       m =>
         val key = m.group(1)
@@ -16,7 +17,6 @@ object VaultReader {
     }
   }
 }
-
 
 object VaultReaderApp extends App {
   VaultReader.readVaultProperties("dev")
